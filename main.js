acf5ffb0d1401cc2c8176e00ec2a311556ba7b67
Igor Rogic <roga.igor@gmail.com>
2022-09-01 16:35:32 +0200

    Add 2 items and request them too

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+let fetchItems = function(items) {
+    for (let item of items) {
+        fetch('/' + item)
+        .then((response) => console.log(response.text()));
+    }
+}
+
 fetch('/list.json')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((response) => console.log(response.json()));
